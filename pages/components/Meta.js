3c1c80c1cd8db3420d0002836c4edd9b2a785e73
Piyush Panchal <77259664+Programmer005@users.noreply.github.com>
2021-10-21 12:01:41 +0530

    :(

--- pages/components/Meta.js
+++ pages/components/Meta.js
@@ -3,27 +3,13 @@ import React from "react"
 export default function Meta() {
   return (
     <>
-    <!-- HTML Meta Tags -->
-  <title>Piyush Panchal</title>
-  <meta name="description" content="Hi I am Piyush Panchal. I am a frontend developer, currently learning backend.">
-
-  <!-- Facebook Meta Tags -->
-  <meta property="og:url" content="https://piyushpanchal.vercel.app/">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="Piyush Panchal">
-  <meta property="og:description" content="Hi I am Piyush Panchal. I am a frontend developer, currently learning backend.">
-  <meta property="og:image" content="https://piyushpanchal.vercel.app/avatar.png">
-
-<!-- Twitter Meta Tags -->
-  <meta name="twitter:card" content="summary_large_image">  
-  <meta property="twitter:domain" content="piyushpanchal.vercel.app">
-  <meta property="twitter:url" content="https://piyushpanchal.vercel.app/">
-  <meta name="twitter:title" content="Piyush Panchal">
-  <meta name="twitter:description" content="Hi I am Piyush Panchal. I am a frontend developer, currently learning backend.">
-  <meta name="twitter:image" content="https://piyushpanchal.vercel.app/avatar.png">
-
-<!-- Meta Tags Generated via https://www.opengraph.xyz -->
-
+      <meta
+        name="description"
+        content="Hi I am Piyush Panchal. I am a frontend developer, currently learning backend."
+      />
+      <meta property="og:image" content="/avatar.png" />
+      <meta property="og:title" content="Piyush Panchal" />
+      <meta property="og:url" content="https://piyushpanchal.vercel.app" />
     </>
   )
 }
